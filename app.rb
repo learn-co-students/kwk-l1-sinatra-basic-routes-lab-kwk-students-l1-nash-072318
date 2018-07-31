48e8b5495e31e094ee5f5f081eb1b9d3d7cf6d32
monet-jones <monet.s.jones@gmail.com>
2018-07-31 17:05:32 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    "hello world"
+  end 
+   
+   get '/name' do
+     "My name is Monet Jones" 
+   end 
+   
+   get '/hometown' do 
+   "My hometown is Florence, Kentucky "
+ end 
+ 
+ get '/favorite-song' do 
+  "My favorite song is Never Growing Up - Avril Lavingne "
+end 
+
 end
